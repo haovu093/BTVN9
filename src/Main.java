5da6c8d961c9b188ee04a1916bc525865411ad09
HaoVT <haovt7@onemount.com>
2021-05-27 08:26:01 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String args[]) {
+        TamGiac tg1 = new TamGiac();
+        tg1.inTamgiac();
+        HinhCN cn1 = new HinhCN();
+        cn1.inHCN();
+    }
+}
+
